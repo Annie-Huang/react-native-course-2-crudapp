5839ab62e3db5392abfdef1ce1c472258972f951
Annie Huang <annie.hn@gmail.com>
2025-12-23 13:12:28 +1100

    Change the Colors value in theme.ts

--- constants/theme.ts
+++ constants/theme.ts
@@ -10,20 +10,22 @@ const tintColorDark = '#fff';
 
 export const Colors = {
   light: {
-    text: '#11181C',
-    background: '#fff',
-    tint: tintColorLight,
-    icon: '#687076',
-    tabIconDefault: '#687076',
-    tabIconSelected: tintColorLight,
+    text: 'black',
+    background: 'white',
+    // tint: tintColorLight,
+    icon: 'black',
+    // tabIconDefault: '#687076',
+    // tabIconSelected: tintColorLight,
+    button: 'royalblue',
   },
   dark: {
-    text: '#ECEDEE',
-    background: '#151718',
-    tint: tintColorDark,
-    icon: '#9BA1A6',
-    tabIconDefault: '#9BA1A6',
-    tabIconSelected: tintColorDark,
+    text: 'white',
+    background: 'black',
+    // tint: tintColorDark,
+    icon: 'red',
+    // tabIconDefault: '#9BA1A6',
+    // tabIconSelected: tintColorDark,
+    button: 'white',
   },
 };
 
@@ -47,7 +49,8 @@ export const Fonts = Platform.select({
   web: {
     sans: "system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Helvetica, Arial, sans-serif",
     serif: "Georgia, 'Times New Roman', serif",
-    rounded: "'SF Pro Rounded', 'Hiragino Maru Gothic ProN', Meiryo, 'MS PGothic', sans-serif",
+    rounded:
+      "'SF Pro Rounded', 'Hiragino Maru Gothic ProN', Meiryo, 'MS PGothic', sans-serif",
     mono: "SFMono-Regular, Menlo, Monaco, Consolas, 'Liberation Mono', 'Courier New', monospace",
   },
 });
