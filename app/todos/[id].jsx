3e98237fe3e2a3497088e97151f3025891ccb8c6
Annie Huang <annie.hn@gmail.com>
2025-12-23 16:03:50 +1100

    Add all the variables in

--- app/todos/[id].jsx
+++ app/todos/[id].jsx
@@ -1,8 +1,16 @@
-import { useLocalSearchParams } from 'expo-router';
+import { useLocalSearchParams, useRouter } from 'expo-router';
 import { Text, View } from 'react-native';
+import { useContext, useState } from 'react';
+import { Inter_500Medium, useFonts } from '@expo-google-fonts/inter';
+import { ThemeContext } from '@/context/ThemeContext';
 
 export default function EditScreen() {
   const { id } = useLocalSearchParams();
+  const [todo, setTodo] = useState({});
+  const { colorScheme, setColorScheme, theme } = useContext(ThemeContext);
+  const router = useRouter();
+
+  const [loaded, error] = useFonts({ Inter_500Medium });
 
   return (
     <View>
