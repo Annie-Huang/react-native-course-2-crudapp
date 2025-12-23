05b0d8058e1848292bac38d52f206cccf017ca0e
Annie Huang <annie.hn@gmail.com>
2025-12-23 15:39:54 +1100

    Create a new route for 'todos/[id]' in _layout.jsx

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -9,6 +9,7 @@ export default function RootLayout() {
         <Stack screenOptions={{ headerShown: false }}>
           {/*<Stack.Screen name='index' options={{ headerShown: false }} />*/}
           <Stack.Screen name='index' />
+          <Stack.Screen name='todos/[id]' />
         </Stack>
       </SafeAreaProvider>
     </ThemeProvider>
